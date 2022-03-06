dc231d820da1865ac790d7b1381ff3668431d758
ushara09 <thimalushara@gmail.com>
2022-03-06 21:30:38 +0530

    Cards

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -25,6 +25,31 @@ class _QuoteListState extends State<QuoteList> {
     Quote("Author05", "Turn your wounds into wisdom"),
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +61,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map((quote) {
-          return Text("${quote.text} - ${quote.author}");
+          return quoteTemplate(quote);
         }).toList(),
       ),
     );
